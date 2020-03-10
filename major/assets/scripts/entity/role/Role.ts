28ea13b943331768224bada58d5ae37351fb5d50
zhicheng <2430455821@qq.com>
2020-03-10 23:35:37 +0800

    添加角色移动限制

--- major/assets/scripts/entity/role/Role.ts
+++ major/assets/scripts/entity/role/Role.ts
@@ -4,40 +4,80 @@ import { RoleData } from "../../data/RoleData";
  * @Author: FeiFan Chen 
  * @Date: 2020-01-19 16:55:30 
  * @Last Modified by: XiongZhiCheng
- * @Last Modified time: 2020-03-06 00:14:39
+ * @Last Modified time: 2020-03-09 00:39:22
  */
 const { ccclass, property } = cc._decorator;
 
+const FIREDELAY = 0.2;
+
 @ccclass
 export class Role extends cc.Component {
+
+    private _timer: number = 0;
+
+    private _widthLimit: number = 0;
+
+    private _heightLimit: number = 0;
 
     private _data: RoleData = null;
     public get data(): RoleData {
         return this._data;
     }
 
-    private _playerWidth: number = 0;
+    public onLoad(): void {
+        this.init();
+    }
+
+    public init(): void {
+        let systemInfo = window.gameCommon.getSDK.getSystemInfo();
+        if (!systemInfo) {
+            this._widthLimit = (640 - this.node.width) / 2;
+            this._heightLimit = (1386 - this.node.height) / 2;
+            return;
+        }
+        let systemHeight = systemInfo.screenHeight;
+        let systemWidth = systemInfo.screenWidth;
+        this._widthLimit = (systemWidth - this.node.width) / 2;
+        this._heightLimit = (systemHeight - this.node.height) / 2;
+    }
 
     public bindData(data: RoleData): void {
         if (!data) {
             return;
         }
         this._data = data;
-        this._playerWidth = this.node.width;
     }
 
     public touchStart(): void {
     }
 
     public updateSelf(dt: number): void {
+        this._timer += dt;
+        if (this._timer > FIREDELAY) {
+            this.fire();
+            this._timer = 0;
+        }
+
         let x = cc.misc.lerp(this.node.position.x, this._data.pos.x, 50 * dt);
         let y = cc.misc.lerp(this.node.position.y, this._data.pos.y, 50 * dt);
-        if (x > 320 - this._playerWidth / 2) {
-            x = 320 - this._playerWidth / 2
-        } else if (x < -320 + this._playerWidth / 2) {
-            x = -320 + this._playerWidth / 2;
+
+        if (x > this._widthLimit) {
+            x = this._widthLimit;
+        } else if (x < -this._widthLimit) {
+            x = -this._widthLimit;
         }
+
+        if (y > this._heightLimit) {
+            y = this._heightLimit;
+        } else if (y < - this._heightLimit) {
+            y = - this._heightLimit;
+        }
+
         this.node.position = cc.v2(x, y);
+    }
+
+    private fire(): void {
+        gameContext.bulletManager.createBullet(this.node);
     }
 
     public touchEnd(): void {
