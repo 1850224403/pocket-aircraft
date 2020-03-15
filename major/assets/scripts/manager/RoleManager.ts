434914aaa88d88ee6b1e688875ca68ab1ab1cc14
zhicheng <2430455821@qq.com>
2020-03-15 17:23:24 +0800

    添加过关

--- major/assets/scripts/manager/RoleManager.ts
+++ major/assets/scripts/manager/RoleManager.ts
@@ -7,7 +7,7 @@ import { AI } from "../entity/ai/AI";
  * @Author: FeiFan Chen 
  * @Date: 2019-12-26 18:52:51 
  * @Last Modified by: XiongZhiCheng
- * @Last Modified time: 2020-03-13 00:44:48
+ * @Last Modified time: 2020-03-15 17:14:22
  */
 export class RoleManager {
 
@@ -16,32 +16,12 @@ export class RoleManager {
         return this._player;
     }
 
-    private _roleList: Set<Role> = new Set();
-
-    private _aiList: Set<AI> = new Set();
-
-    public getRoleByID(id: number): Role {
-        let aimRole = null;
-        this._roleList.forEach((key, role) => {
-            if (role && role.data.id === id) {
-                aimRole = role;
-            }
-        });
-        return aimRole;
-    }
-
     public init(): void {
-        this._roleList = new Set();
         this.createPlayer();
     }
 
     public updateSelf(dt: number): void {
-        this._roleList.forEach((key, role) => {
-            role && role.updateSelf(dt);
-        });
-        this._aiList.forEach((key, ai) => {
-            ai && ai.updateSelf(dt);
-        });
+        this._player.updateSelf(dt);
     }
 
     private createPlayer(): void {
@@ -60,11 +40,8 @@ export class RoleManager {
         player.bindData(roleData);
         gameContext.battleData.playerData = roleData;
         this._player = player;
-        this._roleList.add(player);
     }
 
     public clear(): void {
-        this._roleList.clear();
+        this._player = null;
     }
-
-}
